505d6492f1a05e5171a50f0613edf7cf82fbeb75
Anthony Rosario <anthonymrosario225@gmail.com>
2021-05-18 11:27:13 -0700

    ?

--- src/services/newsApi.js
+++ src/services/newsApi.js
@@ -2,7 +2,7 @@ export const getArticles = async () => {
   const res = await fetch(`https://newsapi.org/v2/everything?q=news&pageSize=5&apiKey=${process.env.NEWS_API_KEY}`);
   const { articles } = await res.json();
 
-  return articles.map(article => ({
+  return articles?.map(article => ({
     source: article.source,
     title: article.title,
     author: article.author,
@@ -16,7 +16,7 @@ export const getSearchedArticle = async(query) => {
   const res = await fetch(`https://newsapi.org/v2/everything?q=${query}&apiKey=${process.env.NEWS_API_KEY}`)
   const { articles } = await res.json();
 
-  return articles.map(article => ({
+  return articles?.map(article => ({
     source: article.source,
     title: article.title,
     author: article.author,
